5dc63663f241c0f3b2ddc9804a940a7648f822ff
Ju Hyung Lee <jufoot@icloud.com>
2017-01-01 13:25:56 +0900

    Download EGL header files

--- genggl.rb
+++ genggl.rb
@@ -574,13 +574,15 @@ puts "Trying to generate GGL based on #{$user_api} #{$user_profile} #{$user_vers
 
 #-------------------------------------------------------------------------------
 
-puts "Downloading Khronos GL header files..."
+puts "Downloading GL header files..."
 
 Dir.mkdir("GL") unless File.exists?("GL")
 File.write("GL/glext.h", Net::HTTP.get(URI.parse("https://www.opengl.org/registry/api/GL/glext.h")))
 File.write("GL/glcorearb.h", Net::HTTP.get(URI.parse("https://www.opengl.org/registry/api/GL/glcorearb.h")))
 File.write("GL/glxext.h", Net::HTTP.get(URI.parse("https://www.opengl.org/registry/api/GL/glxext.h")))
 File.write("GL/wglext.h", Net::HTTP.get(URI.parse("https://www.opengl.org/registry/api/GL/wglext.h")))
+
+puts "Downloading GLES header files..."
 
 Dir.mkdir("GLES") unless File.exists?("GLES")
 File.write("GLES/gl.h", Net::HTTP.get(URI.parse("http://khronos.org/registry/gles/api/GLES/gl.h")))
@@ -600,6 +602,13 @@ File.write("GLES3/gl3platform.h", Net::HTTP.get(URI.parse("http://khronos.org/re
 
 Dir.mkdir("KHR") unless File.exists?("KHR")
 File.write("KHR/khrplatform.h", Net::HTTP.get(URI.parse("http://khronos.org/registry/egl/api/KHR/khrplatform.h")))
+
+puts "Downloading EGL header files..."
+
+Dir.mkdir("EGL") unless File.exists?("EGL")
+File.write("EGL/egl.h", Net::HTTP.get(URI.parse("http://khronos.org/registry/egl/api/EGL/egl.h")))
+File.write("EGL/eglext.h", Net::HTTP.get(URI.parse("http://khronos.org/registry/egl/api/EGL/eglext.h")))
+File.write("EGL/eglplatform.h", Net::HTTP.get(URI.parse("http://khronos.org/registry/egl/api/EGL/eglplatform.h")))
 
 registry_url = "https://cvs.khronos.org/svn/repos/ogl/trunk/doc/registry/public/api"
 
